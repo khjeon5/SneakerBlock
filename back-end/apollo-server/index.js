596f36b1ca73bbf31a660fdfdf5f93ed718e6a4a
khjeon5 <khjeon5328@naver.com>
2020-06-06 22:31:44 +0900

    no message

--- back-end/apollo-server/index.js
+++ back-end/apollo-server/index.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 
 const dbName = "" //DB name
 const dbpassword = ""
-const uri = `mongodb://Sneakeruser:sneker123%21%40%23@cluster0-shard-00-00-aq6wd.mongodb.net:27017,cluster0-shard-00-01-aq6wd.mongodb.net:27017,cluster0-shard-00-02-aq6wd.mongodb.net:27017/sneakers?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`
+const uri = ``
 mongoose.Promise = global.Promise
 mongoose.connect(uri, { useNewUrlParser: true })// { useNewUrlParser: true }
 
@@ -14,7 +14,7 @@ mongoose.connect(uri, { useNewUrlParser: true })// { useNewUrlParser: true }
 const typeDefs = fs.readFileSync(__dirname.concat('/graphql/schema.graphql'), 'utf8')
 
 const schema = makeExecutableSchema({
-  cors: true,
+    cors: true,
     typeDefs,
     resolvers,
 })
