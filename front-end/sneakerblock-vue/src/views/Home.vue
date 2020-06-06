4abc74c5e4ad5c43979ef262fb0c2cd7621731a7
khjeon5 <khjeon5328@naver.com>
2020-06-06 22:29:48 +0900

    20/06/06 connect front and back end by Grey

--- front-end/sneakerblock-vue/src/views/Home.vue
+++ front-end/sneakerblock-vue/src/views/Home.vue
@@ -4,6 +4,12 @@
     <v-container>
       <productAllList />
     </v-container>
+     <div v-for="user in allUsers" :key="user._id">
+      {{user.name}}<br>
+      {{user.email}}<br>
+      {{user.address}}<br>
+      {{user.pubKey}}<br>
+    </div>
   </div>
 </template>
 
@@ -11,8 +17,21 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import productAllList from '@/components/product/productAllList.vue'
+import gql from 'graphql-tag';
 
 export default {
+  apollo:{
+    allUsers : gql`query{
+    allUsers{
+     _id
+     email
+    password
+    name
+    address
+    pubKey
+      }
+    }`
+  },
   name: 'Home',
   components: {
     HelloWorld,
