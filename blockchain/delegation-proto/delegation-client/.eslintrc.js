99b2b2fe5ee822b3d3de34d1fd116f7f9cd59318
elixirevo <elixirevocation@gmail.com>
2020-06-14 22:56:59 +0900

    200614 delegation-proto

---
+++ blockchain/delegation-proto/delegation-client/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        tabWidth: 2,
+        printWidth: 180,
+        trailingComma: 'all',
+        arrowParens: 'avoid',
+      },
+    ],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always',
+      },
+    ],
+  },
+}
