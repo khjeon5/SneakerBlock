effc5b1ef6f8125bd71b933cab3f5bea2cc06928
elixirevo <elixirevocation@gmail.com>
2020-06-26 12:32:12 +0900

    truffle deploy

--- front-end/sneakerblock-vue/migrations/2_token_migration.js
+++ front-end/sneakerblock-vue/migrations/2_token_migration.js
@@ -2,35 +2,29 @@ const Token7 = artifacts.require('Token7')
 const Token17 = artifacts.require('Token17')
 const fs = require('fs')
 
-module.exports = async function (deployer) {
+module.exports = async function(deployer) {
   deployer.deploy(Token7, 'TEST', 'TET', 0, 10000).then(() => {
     if (Token7._json) {
-      fs.writeFile('deployedABIToken7', JSON.stringify(Token7._json.abi, 2), (err) => {
+      fs.writeFile('deployedABIToken7', JSON.stringify(Token7._json.abi, 2), err => {
         if (err) throw err
         console.log(`The abi of ${Token7._json.contractName} is recorded on deployedABI file`)
       })
     }
-    fs.writeFile(
-      'deployedAddressToken7',
-      Token7.address,
-      (err) => {
-        if (err) throw err
-        console.log(`The deployed contract address * ${Token7.address} * is recorded on deployedAddress file`)
+    fs.writeFile('deployedAddressToken7', Token7.address, err => {
+      if (err) throw err
+      console.log(`The deployed contract address * ${Token7.address} * is recorded on deployedAddress file`)
     })
   })
   deployer.deploy(Token17, 'NFTTEST', 'NFTT').then(() => {
     if (Token17._json) {
-      fs.writeFile('deployedABIToken17', JSON.stringify(Token17._json.abi, 2), (err) => {
+      fs.writeFile('deployedABIToken17', JSON.stringify(Token17._json.abi, 2), err => {
         if (err) throw err
         console.log(`The abi of ${Token17._json.contractName} is recorded on deployedABI file`)
       })
     }
-    fs.writeFile(
-      'deployedAddressToken17',
-      Token17.address,
-      (err) => {
-        if (err) throw err
-        console.log(`The deployed contract address * ${Token17.address} * is recorded on deployedAddress file`)
+    fs.writeFile('deployedAddressToken17', Token17.address, err => {
+      if (err) throw err
+      console.log(`The deployed contract address * ${Token17.address} * is recorded on deployedAddress file`)
     })
   })
 }
--- front-end/sneakerblock-vue/truffle.js
+++ front-end/sneakerblock-vue/truffle.js
@@ -1,8 +1,9 @@
+const fs = require('fs')
 const PrivateKeyConnector = require('connect-privkey-to-provider')
 const NETWORK_ID = '1001'
-const GASLIMIT = '20000000'
+const GASLIMIT = '8500000'
 const URL = `https://api.baobab.klaytn.net:8651`
-const PRIVATE_KEY = '0x4d31da58fe94a990d89192c7f77f6e3e056de3ee0bc3188163ca6c957d8776bd'
+const PRIVATE_KEY = fs.readFileSync('./privateKey', 'utf8')
 
 module.exports = {
   networks: {
@@ -11,6 +12,19 @@ module.exports = {
       network_id: NETWORK_ID,
       gas: GASLIMIT,
       gasPrice: null,
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.5.1', // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
     },
   },
 }
