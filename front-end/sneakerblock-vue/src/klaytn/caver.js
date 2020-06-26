8562ca5b05241979b607b0e96fdb87e8703f0061
elixirevo <elixirevocation@gmail.com>
2020-06-26 14:18:58 +0900

    imsi

--- front-end/sneakerblock-vue/src/klaytn/caver.js
+++ front-end/sneakerblock-vue/src/klaytn/caver.js
@@ -1,16 +1,13 @@
 import Caver from 'caver-js'
 import Web3 from 'web3'
 
-const deployedABI = require('./deployedABI.json')
+const deployedABI = require('./deployedABITokne7.json')
 const TEST_NET = 'https://api.baobab.klaytn.net:8651'
-
 export const config = {
   rpcURL: TEST_NET,
 }
-
 const caver = new Caver(new Web3.providers.HttpProvider(config.rpcURL))
-
-const DEPLOYED_ADDRESS = '0xa47898be53fba0fecdb9b4e1dadea5bf0f3c77f7' // testnet
+const DEPLOYED_ADDRESS = '0xf6E1f48CC41Fc55EB67b1119F40a0beaFB4362b6'
 
 const getContractInstance = () => {
   const contractInstance = deployedABI && DEPLOYED_ADDRESS && new caver.klay.Contract(deployedABI, DEPLOYED_ADDRESS)
